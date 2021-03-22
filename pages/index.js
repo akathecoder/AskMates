bf230ef9ae6c5accff93864e22b151290b978e72
rG <12301raghavgoyal@gmail.com>
2021-03-22 14:29:21 +0530

    Install TailwindCSS V2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -11,23 +10,38 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{" "}
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing{" "}
+          <code className={styles.code}>
+            pages/index.js
+          </code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a
+            href="https://nextjs.org/docs"
+            className={styles.card}
+          >
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>
+              Find in-depth information about Next.js
+              features and API.
+            </p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a
+            href="https://nextjs.org/learn"
+            className={styles.card}
+          >
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>
+              Learn about Next.js in an interactive course
+              with quizzes!
+            </p>
           </a>
 
           <a
@@ -35,7 +49,10 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>
+              Discover and deploy boilerplate example
+              Next.js projects.
+            </p>
           </a>
 
           <a
@@ -44,7 +61,8 @@ export default function Home() {
           >
             <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public
+              URL with Vercel.
             </p>
           </a>
         </div>
@@ -56,10 +74,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{" "}
+          <img
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.logo}
+          />
         </a>
       </footer>
     </div>
-  )
+  );
 }
