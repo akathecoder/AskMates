3a70477c1eeea5090dd865335f7368d025b6d1ae
Sparsh Agarwal <sparshaga25@gmail.com>
2021-04-11 10:55:48 +0530

    Added the new Navbar to index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Nav";
 import Footer from "../components/Footer";
 import Link from "next/link";
 
