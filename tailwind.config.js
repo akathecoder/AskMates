9083cde04e5009ed45a78d3b5da8b296109e20bb
Raghav Goyal <12301raghavgoyal@gmail.com>
2021-04-13 20:18:34 +0530

    Extend hover for fontWeight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,29 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        login: "url('/login1.svg')",
-        question404: "url('/assets/caveMan.gif')",
-      }),
-    },
-    fontFamily: {
-      display: [
-        "Roboto",
-        "Ubuntu",
-        "Cantarell",
-        "sans-serif",
-      ],
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			backgroundImage: theme => ({
+				login: "url('/login1.svg')",
+				question404: "url('/assets/caveMan.gif')",
+			}),
+		},
+		fontFamily: {
+			display: [
+				"Roboto",
+				"Ubuntu",
+				"Cantarell",
+				"sans-serif",
+			],
+		},
+	},
+	variants: {
+		extend: {
+			fontWeight: ["hover"],
+		},
+	},
+	plugins: [],
 };
