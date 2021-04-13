50f0cdfeb6e22606a69c322c708a7c5cdb2f52e1
Raghav Goyal <12301raghavgoyal@gmail.com>
2021-04-13 20:17:38 +0530

    Create dedicated /q path

---
+++ pages/q/index.js
@@ -0,0 +1,44 @@
+import Navbar from "../../components/Nav";
+import HomePage from "../../components/QuestionsListPage/QuestionsListPage";
+import axios from "axios";
+
+export default function Home({ questionData }) {
+	return (
+		<div className="">
+			<Navbar />
+			<HomePage questionData={questionData} />
+		</div>
+	);
+}
+
+export async function getServerSideProps() {
+	const questionData = await axios
+		.get("http://localhost:4001/questions")
+		.catch(error => {
+			console.log(error);
+			return;
+		});
+	if (questionData === null) {
+		return {
+			props: {
+				questionData: null,
+			}, // will be passed to the page component as props
+		};
+	}
+	// const answerData = questionData.map(question=>{
+	// 	const answer = await axios.get(
+	// 		"http://localhost:4001/answers/byquestionid/",
+	// 		{
+	// 			params: {
+	// 				questionId: question.questionId,
+	// 			},
+	// 		}
+	// 	);
+	// 	return answer
+	// })
+	return {
+		props: {
+			questionData: JSON.stringify(questionData.data),
+		},
+	};
+}
