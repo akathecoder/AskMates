a0ef004b245fe4ff0fb0f20e32615e1bf8bd7b8a
Sparsh Agarwal <sparshaga25@gmail.com>
2021-04-16 18:12:47 +0530

    Nav Z Index Issue

--- components/QuestionsListPage/QuestionsListPage.js
+++ components/QuestionsListPage/QuestionsListPage.js
@@ -2,15 +2,14 @@ import LeftSidePane from "../LeftSideBar/LeftSidePane";
 import QuestionSet from "./QuestionSet/QuestionSet";
 
 const QuestionsListPage = ({ questionData }) => {
-	return (
-		<section className="md:mx-32 lg:mx-48 pb-20">
-			<div className="fixed top-0 w-full h-40 bg-white z-10"></div>
-			<div className="flex flex-row">
-				<LeftSidePane />
-				<QuestionSet questionData={questionData} />
-			</div>
-		</section>
-	);
+  return (
+    <section className="md:mx-32 lg:mx-48 pb-20 mt-16">
+      <div className="flex flex-row">
+        <LeftSidePane />
+        <QuestionSet questionData={questionData} />
+      </div>
+    </section>
+  );
 };
 
 export default QuestionsListPage;
---
+++ pages/ask.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Nav";
+
+function ask() {
+  return (
+    <>
+      <Navbar />
+      <h1>Ask Your Question</h1>
+    </>
+  );
+}
+
+export default ask;
