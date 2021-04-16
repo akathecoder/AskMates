2ad65a4a3edbca25998d0fae21564713af18fc1c
Raghav Goyal <12301raghavgoyal@gmail.com>
2021-04-16 15:51:09 +0530

    Change variable names

--- components/QuestionsListPage/QuestionSet/QuestionSet.js
+++ components/QuestionsListPage/QuestionSet/QuestionSet.js
@@ -2,7 +2,9 @@ import Question from "./Question.js";
 const QuestionSet = ({ questionData }) => {
 	questionData = JSON.parse(questionData);
 	const questionSet = [];
-	for (const [key, value] of Object.entries(questionData)) {
+	for (const [_key, value] of Object.entries(
+		questionData
+	)) {
 		questionSet.push(value);
 	}
 	const mapQuestion = questionSet.map(question => {
@@ -14,7 +16,12 @@ const QuestionSet = ({ questionData }) => {
 		);
 	});
 	return (
-		<section className="z-0 ml-96">{mapQuestion}</section>
+		<section className="z-0 ml-96">
+			<h2 className="text-3xl font-bold block">
+				Questions
+			</h2>
+			{mapQuestion}
+		</section>
 	);
 };
 
