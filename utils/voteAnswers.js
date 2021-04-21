95a0f82100af22db5b9a1f61e2e8c63fa0275232
Sparsh Agarwal <sparshaga25@gmail.com>
2021-04-21 20:32:06 +0530

    Fixed Voting CORS Bug

--- utils/voteAnswers.js
+++ utils/voteAnswers.js
@@ -5,7 +5,11 @@ export async function voteUp(answerId, votes, setVotes) {
 
   axios
     .patch(
-      process.env.serverUrl + "answers/upvote/" + answerId
+      process.env.serverUrl + "answers/upvote/" + answerId,
+      {},
+      {
+        withCredentials: true,
+      }
     )
     .then(setVotes([votes[0] + 1, votes[1]]))
     .catch((err) => console.error(err));
@@ -14,7 +18,13 @@ export async function voteUp(answerId, votes, setVotes) {
 export async function voteDown(answerId, votes, setVotes) {
   axios
     .patch(
-      process.env.serverUrl + "answers/downvote/" + answerId
+      process.env.serverUrl +
+        "answers/downvote/" +
+        answerId,
+      {},
+      {
+        withCredentials: true,
+      }
     )
     .then(setVotes([votes[0], votes[1] + 1]))
     .catch((err) => console.error(err));
