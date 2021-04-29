da145562857dd2bfab5433dab3b4803fbf4d3fbf
Raghav Goyal <12301raghavgoyal@gmail.com>
2021-04-29 11:09:30 +0530

    Fixed Error: No router instance found

--- pages/forgotPassword/linkSendMessage.js
+++ pages/forgotPassword/linkSendMessage.js
@@ -1,13 +1,16 @@
 import Router from "next/router";
+import { useState, useEffect } from "react";
 
 const linkSendMessage = () => {
+  const [email, setEmail] = useState();
+  useEffect(() => {
+    setEmail(Router.query.email);
+  }, []);
   return (
     <div className="h-screen w-screen flex flex-col">
       <p className="w-full my-auto text-center text-lg">
         Link to reset password has been sent to{" "}
-        <span className="font-bold text-xl">
-          {Router.query.email}
-        </span>
+        <span className="font-bold text-xl">{email}</span>
       </p>
     </div>
   );
